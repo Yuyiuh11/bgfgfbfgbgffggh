a2c26315daafbc07f7b351c4e76fefb7af8d6dc5
Yuyiuh11 <yuyiuh123@gmail.com>
2026-01-31 00:55:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from datetime import datetime
-import requests
-import os
 
 app = Flask(__name__)
-LOG_FILE = "ips.txt"
-
-def ip_info(ip):
-    try:
-        r = requests.get(f"http://ip-api.com/json/{ip}?fields=status,country,regionName,city,isp")
-        data = r.json()
-        if data["status"] == "success":
-            return f'{data["country"]}, {data["regionName"]}, {data["city"]}, {data["isp"]}'
-    except:
-        pass
-    return "No disponible"
 
 @app.route("/")
 def index():
     ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-    ua = request.headers.get("User-Agent")
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    info = ip_info(ip)
+    # (opcional) guardar IPs
+    with open("ips.txt", "a", encoding="utf-8") as f:
+        f.write(f"{time} | {ip}\n")
 
-    with open(LOG_FILE, "a", encoding="utf-8") as f:
-        f.write(f"{time} | {ip} | {info} | {ua}\n")
-
-    return """
-    <h1>Bienvenido</h1>
-    <p>Este sitio registra IPs y ubicación aproximada con fines técnicos.</p>
-    """
+    return render_template("index.html", ip=ip)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
