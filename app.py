ca8ee3c5402231707126f6c46aa7a101ea0f8382
Yuyiuh11 <yuyiuh123@gmail.com>
2026-01-31 01:07:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, request, render_template
-from datetime import datetime
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/ip")
+def get_ip():
     ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # (opcional) guardar IPs
-    with open("ips.txt", "a", encoding="utf-8") as f:
-        f.write(f"{time} | {ip}\n")
-
-    return render_template("index.html", ip=ip)
+    return jsonify({"ip": ip})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
